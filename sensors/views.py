5f082a6e70847ecd3a12a0d96fa6769c3a1d701a
Vadim3x4 <kurnuli2010@yandex.ru>
2021-03-29 00:01:00 +0300

    update_lk

--- sensors/views.py
+++ sensors/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from sensors.models import SensorValues, Sensor, Building
 
+from datetime import datetime as dt
+
 # Create your views here.
 def index(request):
     '''Вьюха отображения главной страницы'''
@@ -11,3 +13,24 @@ def index(request):
     return render(request, 'index.html', context={'data': data,
                                                   # 'buildings':buildings
                                                   })
+
+
+def lk(request):
+    labels = []
+    data = []
+
+    queryset = SensorValues.objects.filter(sensor_id=1)
+    for _ in queryset:
+        labels.append((_.pub_date).strftime('%m/%d/%Y'))
+        data.append(_.value)
+
+
+    print(labels)
+    print(data)
+    return render(
+        request,
+        'lk.html',
+        {
+            'labels': labels,
+            'data': data,
+        })
