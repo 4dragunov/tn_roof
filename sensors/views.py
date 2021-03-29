324025f80675412956f78ff6379b9010b07ccdfa
Vadim3x4 <kurnuli2010@yandex.ru>
2021-03-29 16:04:28 +0300

    upd_lk2

--- sensors/views.py
+++ sensors/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from sensors.models import SensorValues, Sensor, Building
 
-import datetime
+from datetime import date
 import pandas as pd
 import numpy as np
 
@@ -19,44 +19,37 @@ def index(request):
                                                   })
 
 
-def objects_to_df(model, fields=None, exclude=None, date_cols=None, **kwargs):
-    if not fields:
-        fields = [field.name for field in model._meta.get_fields()]
+# def objects_to_df(model, fields=None, exclude=None, date_cols=None, **kwargs):
+#     if not fields:
+#         fields = [field.name for field in model._meta.get_fields()]
 
-    if exclude:
-        fields = [field for field in fields if field not in exclude]
+#     if exclude:
+#         fields = [field for field in fields if field not in exclude]
+#     print(fields)
+#     records = model.objects.filter(**kwargs).values_list(*fields)
+#     df = pd.DataFrame(list(records), columns=fields)
 
-    records = model.objects.filter(**kwargs).values_list(*fields)
-    df = pd.DataFrame(list(records), columns=fields)
 
-    if date_cols:
-        strftime = date_cols.pop(0)
-        for date_col in date_cols:
-            df[date_col] = df[date_col].apply(lambda x: x.strftime(strftime))
+#     if date_cols:
+#         strftime = date_cols.pop(0)
+#         for date_col in date_cols:
+#             df[date_col] = df[date_col].apply(lambda x: x.strftime(strftime))
 
-    return df
-
+#     return df
 
 
 def lk(request):
-    labels = []
-    data = []
 
-    # queryset = SensorValues.objects.filter(sensor_id=1)
-    # for _ in queryset:
-    #     labels.append((_.pub_date).strftime('%m/%d/%Y-%H'))
-    #     data.append(_.value)
+    request_date = date(2021, 3, 28)
+    request_sensor_id = 1
 
-    df = objects_to_df(SensorValues, date_cols=['%H', 'pub_date'])
-    for i in range (0,24):
-        date_filtred = (df[(df["pub_date"] == str(i))])
-        averageNum = date_filtred.iloc[:, 2].mean()
-        labels.append(i)
-        if str(averageNum) == 'nan':
-            data.append(0)
-        else:
-            data.append(averageNum)
-        
+    quaryset = SensorValues.objects.filter(sensor_id=request_sensor_id, pub_date__contains=request_date).values_list()
+    df = pd.DataFrame(list(quaryset), columns=['id', 'sensor', 'value', 'pub_date'])
+
+    labels = [_.strftime("%H:%M:%S") for _ in df['pub_date']]
+    data = [_ for _ in df['value']]
+
+
     return render(
         request,
         'lk.html',
