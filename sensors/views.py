b327c84105aeeee2132725cd56daaf6188ebbe4b
Vadim3x4 <kurnuli2010@yandex.ru>
2021-03-31 03:26:38 +0300

    upd_lk3

--- sensors/views.py
+++ sensors/views.py
@@ -6,7 +6,6 @@ import pandas as pd
 import numpy as np
 
 
-
 def index(request):
     '''Вьюха отображения главной страницы'''
     # получаем список тегов из GET запроса
@@ -18,12 +17,40 @@ def index(request):
                                                   })
 
 
-
-def get_charts(model, filters, df_context):
+def get_charts(model, filters):
+    fields_model = [f.name for f in model._meta.get_fields()]
     quaryset = model.objects.filter(**filters).values_list()
-    df = pd.DataFrame(list(quaryset), **df_context)
+    df = pd.DataFrame(
+        list(quaryset),
+        columns=fields_model
+    )
 
     return df
+
+
+class ChartsData:
+
+    def __init__(self, model, model_filter):
+        self.model = model
+        self.model_filter = model_filter
+        self.df = None
+
+    def get_dataframes(self):
+        fields_model = [f.name for f in self.model._meta.get_fields()]
+        quaryset = self.model.objects.filter(**self.model_filter).values_list()
+        df = pd.DataFrame(list(quaryset), columns=fields_model)
+        self.df = df
+
+    def get_date_kwargs(self, df_key):
+        self.get_dataframes()
+        context_date = list(
+            reversed([_.strftime("%H:%M:%S") for _ in self.df[df_key]]))
+        return context_date
+
+    def get_data_kwargs(self, df_key):
+        self.get_dataframes()
+        context_charts = list(reversed([_ for _ in self.df[df_key]]))
+        return context_charts
 
 
 def lk(request):
@@ -32,75 +59,41 @@ def lk(request):
 
     if not request.GET:
         request_date = datetime.now().date()
-        df = get_charts(
-            SensorValues,
-            {
-                "sensor_id": request_sensor_id,
-                "pub_date__contains": request_date
-            },
-            {
-            'columns':['id', 'sensor', 'value', 'pub_date']
-            }
-        )
-
-        df_temp = get_charts(
-            Weather,
-            {
-                "building": building,
-                "pub_date__contains": request_date
-            },
-            {
-            'columns':['id', 'building', 'temperature', 'snow', 'pub_date']
-            }
-        )
-
 
     if "date" in request.GET:
-        request_date = (datetime.strptime(
-            request.GET['date'],
-            '%Y-%m-%d'
-        )).date()
-        df = get_charts(
-            SensorValues,
-            {
-                "sensor_id": request_sensor_id,
-                "pub_date__contains": request_date
-            },
-            {
-            'columns':['id', 'sensor', 'value', 'pub_date']
-            }
-        )
-        df_temp = get_charts(
-            Weather,
-            {
-                "building": building,
-                "pub_date__contains": request_date
-            },
-            {
-            'columns':['id', 'building', 'temperature', 'snow', 'pub_date']
-            }
-        )
+        if not request.GET['date']:
+            request_date = datetime.now().date()
+        else:
+            request_date = (datetime.strptime(
+                request.GET['date'], '%Y-%m-%d')).date()
 
+    filter_model_sensorsvalues = {
+        "sensor_id": request_sensor_id,
+        "pub_date__contains": request_date
+    }
+    filter_model_weather = {
+        "building": building,
+        "pub_date__contains": request_date
+    }
 
-    labels = list(reversed([_.strftime("%H:%M:%S") for _ in df['pub_date']]))
-    data = list(reversed([_ for _ in df['value']]))
-
-    labels_temp = list(reversed([_.strftime("%H:%M:%S") for _ in df_temp['pub_date']]))
-    data_temp = list(reversed([_ for _ in df_temp['temperature']]))
-
-    labels_snow = list(reversed([_.strftime("%H:%M:%S") for _ in df_temp['pub_date']]))
-    data_snow = list(reversed([_ for _ in df_temp['snow']]))
-
+    charts_sensors_values = ChartsData(
+        SensorValues,
+        filter_model_sensorsvalues
+    )
+    charts_weather_api = ChartsData(
+        Weather,
+        filter_model_weather
+    )
 
     return render(
         request,
         'lk.html',
         {
-            'labels': labels,
-            'data': data,
-            'labels_temp':labels_temp,
-            'data_temp':data_temp,
-            'labels_snow':labels_snow,
-            'data_snow':data_snow
-        })
-
+            'labels': charts_sensors_values.get_date_kwargs('pub_date'),
+            'data': charts_sensors_values.get_data_kwargs('value'),
+            'labels_temp': charts_weather_api.get_date_kwargs('pub_date'),
+            'data_temp': charts_weather_api.get_data_kwargs('temperature'),
+            'labels_snow': charts_weather_api.get_date_kwargs('pub_date'),
+            'data_snow': charts_weather_api.get_data_kwargs('snow')
+        }
+    )
