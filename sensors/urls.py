4cd19c452dbe90ccb380807c322059ef8b5bba60
4dragunov <4dragunov@gmail.com>
2021-04-02 11:44:24 +0300

    charts.js some lines

--- sensors/urls.py
+++ sensors/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 
-from sensors.views import index, lk
+from sensors.views import index, lk, lk2
 
 urlpatterns = [
     path('', index, name='index'),
     path('lk/', lk, name='lk'),
-    path('lk2/<int:pk>', lk, name='lk2'),
+    # path('lk2/<int:pk>', lk, name='lk2'),
+    path('lk2/<int:pk>', lk2, name='lk2'),
 
 ]
