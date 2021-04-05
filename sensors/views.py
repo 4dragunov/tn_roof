ec4fd680fcf1f11e29391ef3f250d6ce48a94175
Vadim3x4 <kurnuli2010@yandex.ru>
2021-04-06 02:24:13 +0300

    upd_sens

--- sensors/views.py
+++ sensors/views.py
@@ -30,40 +30,27 @@ def index(request):
                                                   })
 
 
-class ChartsData:
+def get_dataframes(model, filters):
+    fields_model = [f.name for f in model._meta.get_fields()]
+    quaryset = model.objects.filter(**filters).values_list()
+    df = pd.DataFrame(list(quaryset), columns=fields_model)
+    return df
 
-    def __init__(self, model, model_filter):
-        self.model = model
-        self.model_filter = model_filter
-        self.df = None
 
-    def get_dataframes(self):
-        fields_model = [f.name for f in self.model._meta.get_fields()]
-        quaryset = self.model.objects.filter(**self.model_filter).values_list()
-        df = pd.DataFrame(list(quaryset), columns=fields_model)
-        self.df = df
-
-    def get_date_kwargs(self, df_key):
-        self.get_dataframes()
-        context_date = list(
-            reversed([_.strftime("%H:%M:%S") for _ in self.df[df_key]]))
-        return context_date
-
-    def get_data_kwargs(self, df_key):
-        self.get_dataframes()
-        context_charts = list(reversed([_ for _ in self.df[df_key]]))
-        return context_charts
+def get_df_for_list(df, value, pub_date):
+    date_value = []
+    for i, k in zip(df[pub_date], df[value]):
+        date_value.append({"x": i.strftime("%Y-%m-%d %H:%M:%S"), "y": k})
+    return date_value
 
 
 def lk(request):
     building = 1
-
     sens_id = [i for i in
                list(Sensor.objects.filter(building_id=building).values())]
 
     if not request.GET:
         request_date = datetime.now().date()
-
     if "date" in request.GET:
         if not request.GET['date']:
             request_date = datetime.now().date()
@@ -71,39 +58,36 @@ def lk(request):
             request_date = (datetime.strptime(
                 request.GET['date'], '%Y-%m-%d')).date()
 
-    filter_model_weather = {
-        "building": building,
-        "pub_date__contains": request_date
-    }
-
-    charts_weather_api = ChartsData(
-        Weather,
-        filter_model_weather
-    )
-
     data = []
     for i in sens_id:
         filter_model_sensorsvalues = {
             "sensor_id": int(i['id']),
             "pub_date__contains": request_date
         }
-        charts_sensors_values = ChartsData(
-            SensorValues,
-            filter_model_sensorsvalues
-        )
-        data.append(
-            [i['sens_uid'], charts_sensors_values.get_data_kwargs('value')])
+        dataframe = get_dataframes(SensorValues, filter_model_sensorsvalues)
+        x = get_df_for_list(dataframe, 'value', 'pub_date')
+        data.append([i['sens_uid'], x])
+
+
+    filter_model_weather = {
+        "building": building,
+        "pub_date__contains": request_date
+    }
+    dataframe_weather = get_dataframes(Weather, filter_model_weather)
+    temperature = get_df_for_list(dataframe_weather,'temperature','pub_date')
+    snow = get_df_for_list(dataframe_weather,'snow','pub_date')
+
+
+
+
 
     return render(
         request,
         'lk.html',
         {
             'data': data,
-            'labels': charts_sensors_values.get_date_kwargs('pub_date'),
-            'labels_temp': charts_weather_api.get_date_kwargs('pub_date'),
-            'data_temp': charts_weather_api.get_data_kwargs('temperature'),
-            'labels_snow': charts_weather_api.get_date_kwargs('pub_date'),
-            'data_snow': charts_weather_api.get_data_kwargs('snow')
+            'temperature':temperature,
+            'snow':snow
         }
     )
 
@@ -131,7 +115,6 @@ def lk2(request, building_id):
     #     sensor = get_object_or_404(Sensor, pk=sensor_pk)
     #     value = load_form.data['value']
     #     sensor.max_value = int(value)
-
 
     if not request.GET:
         request_date = datetime.now().date()
