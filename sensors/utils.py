c4de974ee88ff01d5c5695402ae6164bbaccf549
4dragunov <4dragunov@gmail.com>
2021-04-06 16:29:01 +0300

    twilio

--- sensors/utils.py
+++ sensors/utils.py
@@ -3,13 +3,15 @@ import os
 from django.shortcuts import get_object_or_404
 from twilio.rest import Client
 from dotenv import load_dotenv
+
 load_dotenv()
 from sensors.models import Sensor
 import logging
+
 logging.basicConfig(filename="main.log", level=logging.DEBUG)
 
-def check_max_value(sensor, value):
 
+def check_max_value(sensor, value):
     max_value = sensor.max_value
     print(max_value)
     if value > max_value:
@@ -17,18 +19,15 @@ def check_max_value(sensor, value):
                    f' {sensor.sens_uid} на '
                    f'объекте {sensor.building.title}'
                    f' {value} кг/м2. ' \
-               f'Допустимая нагрузка {max_value} кг/м2')
+                   f'Допустимая нагрузка {max_value} кг/м2')
         number = sensor.building.phone_number
         sms_sender(text, number)
 
 
 def sms_sender(sms_text, number):
     print('Sms send complite!')
-    account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-    auth_token = os.getenv('TWILIO_AUTH_TOKEN')
-    # account_sid = os.getenv('twilio_account_sid')
-    # auth_token = os.getenv('twilio_auth_token')
-    # account_sid = {sectet.}
+    account_sid = os.environ.get('twilio_account_sid')
+    auth_token = os.environ.get('twilio_auth_token')
     client = Client(account_sid, auth_token)
     message = client.messages.create(
         body=sms_text,
