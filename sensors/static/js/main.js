a5ab51c7a4c1c2438380331803b0ef3c18664453
4dragunov <4dragunov@gmail.com>
2021-05-10 18:34:57 +0300

    js fix

--- sensors/static/js/main.js
+++ sensors/static/js/main.js
@@ -1,10 +1,13 @@
 $('.part').hover (
 	function() {
-		$('.description_svg').html($(this).attr('description-data'));
-		$('.description_svg').fadeIn();
+		$('.description_svg ').html($(this).attr('description-data'));
+		$('.description_svg ').css({'display':'block'});
+		// $('.description_svg ').fadeIn();
+
 	},
 	function() {
-		$('.description_svg').fadeOut(50);
+		$('.description_svg ').css({'display':'none'});
+		// $('.description_svg ').fadeOut(100);
 	}
 )
 
